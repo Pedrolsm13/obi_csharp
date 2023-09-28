ce9d6c5dc2d0763312b74b38fcc7fe453e5bcdc5
Pedro Lucas <plserramoura@gmail.com>
2023-09-28 17:19:33 -0300

    correção etiquetas

--- etiquetas/main.cs
+++ etiquetas/main.cs
@@ -17,7 +17,62 @@ class Program
     string[] arrString = Console.ReadLine().Split(' ');
     int[] array = new int[ArrLenth];
     //preenche o vetor array conforme entrada de dados, valores da fita
-    for (int i = 0; i < arrString.Length; i++){
+    for (int i = 0; i < ArrLenth; i++){
+      array[i] = int.Parse(arrString[i]);
+    }
+
+    int menor = 10001, indice = -1;
+
+    for (int j = 0; j < nEtiquetas; j++){  
+      for (int i = 0; i < array.Length - (TamEtiqueta - 1); i++){
+        if (somatorio(array, TamEtiqueta, i) < menor){
+          menor = somatorio(array, TamEtiqueta, i);
+          indice = i;
+        }
+      }
+      for (int k = 0; k < TamEtiqueta; k++){
+        array[indice++] = 10001;
+      }
+      menor = 10001;
+    }     
+    int res=0;
+    for(int i = 0; i < ArrLenth; i++){
+      if (array[i] == 10001){
+        array[i] = 0;
+      }
+      res += array[i];
+    }
+    Console.WriteLine(res);
+  }
+  static int somatorio(int[] array, int TamEtiqueta, int i){
+    int soma = 0;
+    for(int j = i; j < TamEtiqueta+i; j++){
+      soma += array[j];
+    }
+    return soma;
+  }
+}
+//impossivel com essa configuação, aceita em apenas um modelo de entrada
+/*using System;
+
+class Program
+
+{
+  public static void Main(string[] args){
+    // Entrada de dados
+    int ArrLenth;
+    int nEtiquetas;
+    int TamEtiqueta;
+    // tamanho da fita, qtd etiquetas e tamanho etiqueta
+    string[] entrada = Console.ReadLine().Split(' ');
+    ArrLenth = int.Parse(entrada[0]);
+    nEtiquetas = int.Parse(entrada[1]);
+    TamEtiqueta = int.Parse(entrada[2]);
+  
+    string[] arrString = Console.ReadLine().Split(' ');
+    int[] array = new int[ArrLenth];
+    //preenche o vetor array conforme entrada de dados, valores da fita
+    for (int i = 0; i < ArrLenth; i++){
       array[i] = int.Parse(arrString[i]);
     }
     //cria vetor para guardar somatorio de posições
@@ -53,4 +108,4 @@ class Program
     }
     Console.WriteLine(res);
   }
-}
+}*/
